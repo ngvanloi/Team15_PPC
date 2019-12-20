8d286137820d83f8a39cc373be3282215293617d
ngvanloi <nguyenloi.itse@gmail.com>
2019-12-20 20:03:18 +0700

    initial commit

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        PPCDBEntities model = new PPCDBEntities();
+        public ActionResult Index()
+        {
+            var p = model.Properties.ToList();
+            return View(p);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Search(string search)
+        {
+            var p = model.Properties.Where(x => x.Property_Name.Contains(search)).ToList();
+            return View(p);
+        }
+       
+
+        
+    }
+}
---
+++ WebApplication1/WebApplication1/Controllers/PropertiesController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+using System.IO;
+namespace WebApplication1.Controllers
+{
+    public class PropertiesController : Controller
+    {
+        private PPCDBEntities model = new PPCDBEntities(); 
+        // GET: Properties
+        public ActionResult Index()
+        {
+            var properties = model.Properties.Include(x => x.District).Include(x => x.Property_Status).Include(x => x.Property_Type);
+            return View(properties.ToList());
+        }
+        public void PopularData(object propertyTypeSelected = null,object propertyStatusSelected = null,object citySelected = null,object districtSelected = null)
+        {
+            ViewBag.Property_Type_ID = new SelectList(model.Property_Type.ToList(), "ID", "Property_Type_Name", propertyTypeSelected);
+            ViewBag.Property_Status_ID = new SelectList(model.Property_Status.ToList(), "ID", "Property_Status_Name", propertyStatusSelected);
+            ViewBag.City_ID = new SelectList(model.Cities.ToList(), "ID", "City_Name", citySelected);
+            ViewBag.District_ID = new SelectList(model.Districts.ToList(), "ID", "District_Name", districtSelected);
+        }
+        [HttpGet]
+        public ActionResult Details(int? id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var property = model.Properties.Find(id);
+            if(property == null)
+            {
+                return HttpNotFound();
+            }
+            return View(property);
+        }
+    }
+}
